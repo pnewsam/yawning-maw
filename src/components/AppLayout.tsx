b5cf0646f1276115f55fe4a92453f46ae33b06e1
pnewsam <paul.newsam@gmail.com>
2023-07-30 09:42:41 -0700

    Init shadcn/ui.

---
+++ src/components/AppLayout.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { Navbar } from "./Navbar";
+import { useToast } from "@/components/ui/use-toast";
+import { Toaster } from "@/components/ui/toaster";
+import clsx from "clsx";
+
+type Props = {
+  children: React.ReactNode;
+  className?: string;
+};
+
+export let showToast: any = undefined;
+
+export const BaseAppLayout = ({ children, className }: Props) => {
+  const { toast } = useToast();
+
+  if (!showToast) {
+    showToast = toast;
+  }
+
+  return (
+    <div className={clsx("min-h-full", className)}>
+      <Navbar />
+      {children}
+      <Toaster />
+    </div>
+  );
+};
+
+export const AppLayout = ({ className, children }: Props) => (
+  <BaseAppLayout className={className}>{children}</BaseAppLayout>
+);
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+export const Navbar = () => {
+  return (
+    <nav className="border-b border-gray-200 w-full py-4 px-8 flex items-center justify-between">
+      <div className="">
+        {/* Left Section */}
+        <span>Yawning Maw</span>
+      </div>
+
+      <div className="">
+        {/* Right Section */}
+        <span>Right</span>
+      </div>
+    </nav>
+  );
+};
