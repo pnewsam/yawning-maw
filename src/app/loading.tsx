7690f0cb0c1086ccb8d9974ce8fe4172b8af2342
pnewsam <paul.newsam@gmail.com>
2023-08-01 12:04:08 -0700

    Fix padding on loading state.

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,21 +1,5 @@
-"use client";
-
-import { Skeleton } from "@/components/ui";
-import { GridLayout } from "@/components/restaurantIndex/GridLayout";
+import { Loading as LoadingIndex } from "@/components/restaurantIndex/_loading";
 
 export default function Loading() {
-  return (
-    <>
-      <GridLayout>
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((_, i) => (
-          <div key={i} className="w-full sm:w-auto">
-            <Skeleton className="sm:w-[300px] max-w-full min-h-[160px] h-auto max-h-[200px] mb-2" />
-            <Skeleton className="w-[80px] max-w-full h-[16px] mb-2" />
-            <Skeleton className="w-[220px] max-w-full h-[16px]" />
-          </div>
-        ))}
-      </GridLayout>
-      <hr className="py-12 w-full block border-none" />
-    </>
-  );
+  return <LoadingIndex />;
 }
