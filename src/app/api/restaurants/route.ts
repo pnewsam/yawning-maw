7f8f3e6fe526a34148f41868d9e416584d869c73
pnewsam <paul.newsam@gmail.com>
2023-08-03 21:36:54 -0700

    Build step workaround

--- src/app/api/restaurants/route.ts
+++ src/app/api/restaurants/route.ts
@@ -1,11 +1,15 @@
 import { NextRequest, NextResponse } from "next/server";
+import { cache } from "react";
 import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 
-export const dynamic = "force-dynamic";
+export const createRouteHandlerSupabaseClient = cache(() => {
+  const cookieStore = cookies();
+  return createRouteHandlerClient({ cookies: () => cookieStore });
+});
 
 export async function GET() {
-  const supabase = createRouteHandlerClient({ cookies });
+  const supabase = createRouteHandlerSupabaseClient();
 
   const { data: restaurants = [], error } = await supabase
     .from("restaurants")
@@ -22,7 +26,7 @@ export async function GET() {
 }
 
 export async function POST(request: NextRequest) {
-  const supabase = createRouteHandlerClient({ cookies });
+  const supabase = createRouteHandlerSupabaseClient();
 
   const { name } = await request.json();
   const { data: restaurant, error } = await supabase
