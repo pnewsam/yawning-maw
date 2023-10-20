84e5557a0505b8dadc49497e98fddf71eab2cd96
pnewsam <paul.newsam@gmail.com>
2023-10-20 15:38:06 -0700

    Remove details page.

--- src/app/restaurants/[slug]/page.tsx
+++
@@ -1,7 +0,0 @@
-import { RestaurantDetails } from "@/components/restaurantDetails/_main";
-import { mockRestaurantRecord, RestaurantTransformer } from "@/models";
-
-export default function RestaurantDetailsPage() {
-  const r = RestaurantTransformer(mockRestaurantRecord);
-  return <RestaurantDetails restaurant={r} />;
-}
